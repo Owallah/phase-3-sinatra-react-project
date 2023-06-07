461fe49bb15560932f420cf7f6f2c795dbdb2cf7
Owallah <mikesonowallah@gmail.com>
2023-06-07 22:03:46 +0300

    Added some fake random data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,44 @@
+# require 'faker'
 puts "🌱 Seeding spices..."
 
 # Seed your database here
 puts "🌱 Seeding art"
 30.times do
     Art.create(
-        title: Faker::Art.title,
+        title: Faker::Lorem.sentence,
         artist_id: rand(1..10),
         review_id: rand(1..10),
-        image_url: Faker::Art.image_url,
-        description: Faker::Art.description
+        image_url: Faker::Internet.url,
+        description: Faker::Lorem.paragraph
     )
 end
 
 puts "🌱 Seeding reviews"
 10.times do
-    Review.create
+    Review.create(
+        star_rating: Faker::Number.between(from: 1, to: 5),
+        comments: Faker::Lorem.paragraph,
+        viewer_id: rand(1..20)
+    )
+end
+
+puts "🌱 Seeding viewers"
+20.times do
+    Viewer.create(
+        name: Faker::Name.name,
+        country_of_origin: Faker::Address.country,
+        profile_pic_url: Faker::Internet.url
+    )
+end
+
+puts "🌱 Seeding artists"
+
+10.times do
+    Artist.create(
+        name: Faker::Artist.name,
+        country_of_origin: Faker::Address.country,
+        avatar_url: Faker::Internet.url
+    )
 end
 
 puts "✅ Done seeding!"
